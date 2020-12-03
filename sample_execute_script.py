ee45043a2fc427fa8795d23173e612448953ca9b
clivorsi <75181301+clivorsi@users.noreply.github.com>
2020-12-03 14:30:53 -0700

    Create sample_execute_script.py

---
+++ sample_execute_script.py
@@ -0,0 +1,10 @@
+# You'll need to modify the data_directory to point to the location you are dropping your files and your achive_directory
+to where you want to files to go after processing.
+
+
+from budget_extraction import extract_budget_data
+
+data_directory = 'C:\DOE Project\\'
+archive_directory = 'C:\DOE Project\Archive\\'
+
+extract_budget_data(data_directory,archive_directory)
